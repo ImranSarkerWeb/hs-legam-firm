7a445e1087eab40067d9c636f1a6feca116e2a07
safiislam <118913244+safiislam@users.noreply.github.com>
2023-12-09 22:16:37 +0600

    add

--- src/Pages/About/AboutComponets/Banner.jsx
+++ src/Pages/About/AboutComponets/Banner.jsx
@@ -2,11 +2,11 @@
 const Banner = ({title ,discripction}) => {
 
     return (
-        <div className=" w-full h-[400px]  mt-[80px] bg-cover  bg-aboutBanner">
-            <div className="h-full w-full backdrop-blur-xl text-white bg-black bg-opacity-20 ">
+        <div className=" w-full h-[400px] mt-[80px] bg-cover  bg-aboutBanner">
+            <div className="h-full w-full backdrop-blur-xl px-5 text-white bg-black bg-opacity-20 ">
                 <div className="flex flex-col h-full items-center justify-center">
-                    <p className="text-4xl font-bold text-primery mb-3" >{title}</p>
-                    <p className="text-lg">{discripction}</p>
+                    <p className="text-2xl md:text-4xl font-bold text-primery mb-3 text-center" >{title}</p>
+                    <p className="text-sm md:text-lg text-center">{discripction}</p>
                 </div>
             </div>
         </div>
